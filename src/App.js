d9584fd846b055dfcbda8d1d8323e1f4a1b82d9b
gmaralan <gmartinialan@gmail.com>
2023-01-11 15:52:17 -0400

    finished req 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Login from './Pages/Login';
+import Feedbacks from './Pages/Feedbacks';
+import Settings from './Pages/Settings';
+import Ranking from './Pages/Ranking';
+import Game from './Pages/Game';
 
 export default function App() {
   return (
@@ -8,6 +14,11 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>SUA VEZ</p>
+        <Route exact path="/" render={ () => <Login /> } />
+        <Route path="/game" render={ () => <Game /> } />
+        <Route path="/settings" render={ () => <Settings /> } />
+        <Route path="/feedbacks" render={ () => <Feedbacks /> } />
+        <Route path="/ranking" render={ () => <Ranking /> } />
       </header>
     </div>
   );
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,20 +1,59 @@
 // import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import React from 'react';
+import React, { Component } from 'react';
 
-function Login() {
-  // const { prop1, dispatch } = props;
-  return (
-    <div>content</div>
-  );
+class Login extends Component {
+  state = {
+    isButtonInvalid: true,
+    email: '',
+    name: '',
+  };
+
+  validateButton = () => {
+    const { email, name } = this.state;
+    if (email.length > 0 && name.length > 0) {
+      this.setState({
+        isButtonInvalid: false,
+      });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.validateButton();
+  };
+
+  render() {
+    const { name, email, isButtonInvalid } = this.state;
+    return (
+      <div>
+        <input
+          type="email"
+          data-testid="input-gravatar-email"
+          name="email"
+          value={ name }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          data-testid="input-player-name"
+          name="name"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          // onClick={ startGame }
+          data-testid="btn-play"
+          disabled={ isButtonInvalid }
+        >
+          Play
+        </button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-  ...state,
-});
-
-// Login.propTypes = {
-//   prop1: PropTypes.string.isRequired,
-//   dispatch: PropTypes.func.isRequired,
-// };
-export default connect(mapStateToProps)(Login);
+export default Login;
